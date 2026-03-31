6b164ff530b863e7fd968275182ae19577908019
haifaamalinawahdan <haifanew3@gmail.com>
2026-03-31 09:02:01 +0700

    fungsi bilangan prima (pengecekan pembagi)

--- pengecekan_bilangan.cpp
+++ pengecekan_bilangan.cpp
@@ -18,4 +18,11 @@ bool Prima(int n)
     if (n <= 1){
         return false;
     }
+
+    int i = 2;
+    while (i <= n/2){
+        if (n % i == 0){
+            return false;
+        }
+    }
 }
